9efdfed965eb9e85172e679924e9e2723fcc2ddc
BDBluhm <bbluhm13@gmail.com>
2025-05-08 16:36:42 -0500

    Added README

--- Backend/Player.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const playerSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Player', playerSchema);
